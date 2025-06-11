645acf948aa0689d3d232e8d3b90064cbea8dc02
moslehSN <mosleh.snoussi49@gmail.com>
2025-06-11 20:30:32 +0200

    fix : [aic-49]-fix-eslint-check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,19 @@ import typescript from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
 import prettierPlugin from "eslint-plugin-prettier";
 import jsdocPlugin from "eslint-plugin-jsdoc";
+import jestPlugin from "eslint-plugin-jest";
 
 export default [
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "coverage/**",
+    ]
+  },
+
   js.configs.recommended,
+
   {
     files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
@@ -13,10 +23,6 @@ export default [
       parserOptions: {
         ecmaVersion: 2021,
         sourceType: "module",
-      },
-      globals: {
-        process: "readonly",
-        console: "readonly",
       },
     },
     plugins: {
@@ -27,15 +33,48 @@ export default [
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
-      camelcase: ["error", { properties: "always" }],
+      "camelcase": ["error", { properties: "always" }],
       "prettier/prettier": "error",
       "jsdoc/check-tag-names": [
         "error",
-        {
-          definedTags: ["swagger"],
-        },
+        { "definedTags": ["swagger"] }
       ],
       "jsdoc/require-description": "error",
+    },
+  },
+
+  {
+    files: [
+      "src/api_documentation/swagger.ts",
+      "src/index.ts"
+    ],
+    languageOptions: {
+      globals: {
+        __dirname: "readonly",
+        process: "readonly",
+        console: "readonly"
+      }
+      
+    }
+  },
+
+  {
+    files: [
+      "**/*.test.ts",
+      "**/*.test.js",
+      "**/*.spec.ts",
+      "**/*.spec.js",
+      "**/__mocks__/**/*.ts",
+      "**/__mocks__/**/*.js",
+    ],
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: jestPlugin.environments.globals.globals,
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
     },
   },
 ];
